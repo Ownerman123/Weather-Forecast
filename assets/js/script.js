6823283759cf112abda50df7285b15b1ab3ccebc
Ownerman123 <charlesshumway1@gmail.com>
2024-04-07 08:32:03 -0600

    bug fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 const apiKey = "bb50796aabd4d5ec717c3afc5dc09b4f";
 const wrongRichfield = `https://api.openweathermap.org/data/2.5/forecast?q=Richfield,Utah,USA&appid=${apiKey}&cnt=5&units=imperial`
  
-const cityList = fetchJSONData();
+// const cityList = fetchJSONData();
 
-console.log(cityList);
+// console.log(cityList);
 fetch(`https://api.openweathermap.org/data/2.5/forecast?id=5545710&appid=${apiKey}&cnt=5&units=imperial`).then(function (response) {
 
    return response.json();
@@ -18,17 +18,17 @@ function GetWeatherData(location) {
 }
 
 
-function fetchJSONData() {
-    fetch("./jsons/citylist.json")
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error
-                    (`HTTP error! Status: ${res.status}`);
-            }
-            return res.json();
-        })
-        .then((data) => 
-              console.log(data))
-        .catch((error) => 
-               console.error("Unable to fetch data:", error));
-}
+// function fetchJSONData() {
+//     fetch("./jsons/citylist.json")
+//         .then((res) => {
+//             if (!res.ok) {
+//                 throw new Error
+//                     (`HTTP error! Status: ${res.status}`);
+//             }
+//             return res.json();
+//         })
+//         .then((data) => 
+//               console.log(data))
+//         .catch((error) => 
+//                console.error("Unable to fetch data:", error));
+// }
