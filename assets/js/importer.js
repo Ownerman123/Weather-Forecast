76d3f11482b5de269f07d28dcb87a0044144fec1
Ownerman123 <charlesshumway1@gmail.com>
2024-04-07 08:36:54 -0600

    filter out US cities

--- assets/js/importer.js
+++ assets/js/importer.js
@@ -3,3 +3,15 @@
 
 import cityList from './jsons/citylist.json' with {type: 'json'}
 console.log(cityList);
+
+
+let usCities = [];
+
+
+for(city of cityList) {
+    if (city.country === "US") {
+        usCities.push(city);
+    }
+}
+
+console.log(usCities);
